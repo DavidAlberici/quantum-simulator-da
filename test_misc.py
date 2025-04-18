b58cd18233e4d6c75796516d5fecbb8d10c61695
David Alberici <davidalberici3200@gmail.com>
2025-04-18 13:34:50 +0200

    Created possible improvement to the way I parse the state vector (for now just implemented in test_misc.py file)

---
+++ test_misc.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+class TestMisc(TestCase):
+    def test__parse_state_vector_positions_where_target_qbit_is_one(self):
+        qbit = 2
+        total_qbits = 3
+        batch_size = int(2 ** (qbit + 1))
+        half_batch_size = int(batch_size / 2)
+        total_states = 2 ** total_qbits
+        for batch_first_position in range(0, total_states, batch_size):
+            first_value_one = batch_first_position + half_batch_size
+            for j in range(first_value_one, first_value_one + half_batch_size, 1):
+                print(j)
