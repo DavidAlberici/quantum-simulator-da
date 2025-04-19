4d6a45183377b89e0a13ea02b08471e1dcaa44b1
David Alberici <davidalberici3200@gmail.com>
2025-04-19 07:25:24 +0200

    Moved all "sketches" to test_misc class

--- test_misc.py
+++ test_misc.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
+import numpy as np
+import scipy as sp
+import gates
 
 class TestMisc(TestCase):
+    """
+    This class can be ignored, is just like a "notebook" to make drafts/sketches
+    """
     def test__parse_state_vector_positions_where_target_qbit_is_one(self):
         qbit = 2
         total_qbits = 3
@@ -27,3 +33,22 @@ class TestMisc(TestCase):
             for j in range(first_value_one, first_value_one + half_batch_size, 1):
                 total_prob += self.value_prob(j)
         return total_prob
+
+    @staticmethod
+    def basic_sparse_array_methods():
+        arr = sp.sparse.csr_array((2, 2))  # create 2x2 matrix
+        arr[(1, 0)] = 1  # assign
+        print(arr)
+        print("*******")
+        print(arr.toarray())  # convert to normal array
+        print("*******")
+        nparr = np.eye(8)
+        scarr = sp.sparse.csr_array(nparr)  # support conversion from np to sp.csr
+        print(scarr)
+
+    @staticmethod
+    def kron_in_sparse_array():
+        h_sparse = sp.sparse.csr_array(gates.h)
+        i_sparse = sp.sparse.csr_array(gates.I(1))
+        print(sp.sparse.kron(h_sparse, i_sparse))
+        # print(np.kron(h_sparse, i_sparse))
